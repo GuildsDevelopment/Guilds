17497df174da54cb3bb0ad48b01f596e2277b9ca
Jacksonnn <jhuse01@gmail.com>
2018-05-01 22:04:18 -0400

    Create ConfigManager.java

---
+++ com/Jacksonnn/Guilds/configuration/ConfigManager.java
@@ -0,0 +1,30 @@
+package com.Jacksonnn.Guilds.Configuration;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class ConfigManager {
+	public static Config defaultConfig;
+	public static Config language;
+	
+	public ConfigManager() {
+		defaultConfig = new Config(new File("config.yml"));
+		language = new Config(new File("language.yml"));
+	}
+	
+	public static void configCheck(ConfigType type) {
+		FileConfiguration config;
+		if (type == ConfigType.LANGUAGE) {
+			config = language.get();
+			
+			config.addDefault("General.prefix", "&8[&2Guilds&8]&f ");
+			config.addDefault("Commands.NoPermission", "You do not have permission to do that.");
+			
+		} else if (type == ConfigType.DEFAULT) {
+			config = defaultConfig.get();
+			
+			
+		}
+	}
+}
