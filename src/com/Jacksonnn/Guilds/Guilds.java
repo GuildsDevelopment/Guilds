0c8f256702869982829584b66424e31eceaf8193
Jacksonnn <jhuse01@gmail.com>
2018-05-10 00:06:51 -0400

    Database pt. 3 (FINAL)
    Finished setting up the database connection and table setup in the database (I think)...
    
    I also added alias commands such as: /guilds, /g, and /guild.

--- src/com/Jacksonnn/Guilds/Guilds.java
+++ src/com/Jacksonnn/Guilds/Guilds.java
@@ -4,13 +4,25 @@ import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.Jacksonnn.Guilds.storage.DBConnection;
+
 public class Guilds extends JavaPlugin {
 	public static Guilds plugin;
 	public static Logger log;
 	
 	@Override
 	public void onEnable() {
-		
+		DBConnection.host = getConfig().getString("Storage.MySQL.host");
+		DBConnection.port = getConfig().getInt("Storage.MySQL.port");
+		DBConnection.pass = getConfig().getString("Storage.MySQL.pass");
+		DBConnection.db = getConfig().getString("Storage.MySQL.db");
+		DBConnection.user = getConfig().getString("Storage.MySQL.user");
+		DBConnection.init();
+		if (DBConnection.isOpen() == false) {
+			// Message is logged by DBConnection
+			return;
+		}
+
 	}
 	
 	@Override
@@ -20,5 +32,7 @@ public class Guilds extends JavaPlugin {
 	
 	public void registerCommands() {
 		this.getCommand("guilds").setExecutor(new Commands());
+		this.getCommand("g").setExecutor(new Commands());
+		this.getCommand("guild").setExecutor(new Commands());
 	}
 }
--- src/com/Jacksonnn/Guilds/storage/DBConnection.java
+++ src/com/Jacksonnn/Guilds/storage/DBConnection.java
@@ -1,5 +1,11 @@
 package com.Jacksonnn.Guilds.storage;
 
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import com.Jacksonnn.Guilds.Guilds;
+import com.Jacksonnn.Guilds.Methods;
+
 /*
  * What needs to be stored:
  * 		Guild:
@@ -21,5 +27,65 @@ package com.Jacksonnn.Guilds.storage;
  */
 
 public class DBConnection {
-	
+
+	public static Database sql;
+
+	public static String host;
+	public static int port;
+	public static String db;
+	public static String user;
+	public static String pass;
+	public static boolean isOpen = false;
+
+	public static void init() {
+		if (Guilds.plugin.getConfig().getString("Storage.engine").equalsIgnoreCase("mysql")) {
+			sql = new MySQL(Guilds.log, "Establishing MySQL Connection...", host, port, user, pass, db);
+			if (((MySQL) sql).open() == null) {
+				Guilds.log.severe("Disabling due to database error");
+				Methods.stopPlugin();
+				return;
+			}
+
+			isOpen = true;
+			Guilds.log.info("Database connection established.");
+
+			if (!sql.tableExists("guilds_guilds")) {
+				Guilds.log.info("Creating guilds_guilds table");
+				String query = "CREATE TABLE `guilds_guilds` (" + "`id` INTEGER NOT NULL," + "`leader` varchar(255) NOT NULL," + "`name` varchar(255)," + "`members` varchar(255)," + "`chunks_claimed` varchar(255)," + "`coins` varchar(255)," + "`apprentices` varchar(255)," + "`trusted` varchar(255)," + " PRIMARY KEY (id));";
+				sql.modifyQuery(query, false);
+			}
+
+			if (!sql.tableExists("guilds_players")) {
+				Guilds.log.info("Creating guilds_players table");
+				String query = "CREATE TABLE `guilds_players` (" + "`uuid` varchar(36) NOT NULL," + "`name` varchar(255) NOT NULL," + "`balance` varchar(255)," + "`previous` varchar(255)," + "`current` varchar(255)," + "`rank` varchar(255)," + "`joined` varchar(255)," + "``last_login varchar(255)," + " PRIMARY KEY (uuid, name));";
+				sql.modifyQuery(query, false);
+			}
+		} else {
+			sql = new SQLite(Guilds.log, "Establishing SQLite Connection.", "Guilds.db", Guilds.plugin.getDataFolder().getAbsolutePath());
+			if (((SQLite) sql).open() == null) {
+				Guilds.log.severe("Disabling due to database error");
+				Methods.stopPlugin();
+				return;
+			}
+
+			isOpen = true;
+			Guilds.log.info("Database connection established.");
+
+			if (!sql.tableExists("guilds_guilds")) {
+				Guilds.log.info("Creating guilds_guilds table");
+				String query = "CREATE TABLE `guilds_guilds` (" + "`id` INTEGER NOT NULL," + "`leader` varchar(255) NOT NULL," + "`name` varchar(255)," + "`members` varchar(255)," + "`chunks_claimed` varchar(255)," + "`coins` varchar(255)," + "`apprentices` varchar(255)," + "`trusted` varchar(255)," + " PRIMARY KEY (id));";
+				sql.modifyQuery(query, false);
+			}
+
+			if (!sql.tableExists("guilds_players")) {
+				Guilds.log.info("Creating guilds_players table");
+				String query = "CREATE TABLE `guilds_players` (" + "`uuid` varchar(36) NOT NULL," + "`name` varchar(255) NOT NULL," + "`balance` varchar(255)," + "`previous` varchar(255)," + "`current` varchar(255)," + "`rank` varchar(255)," + "`joined` varchar(255)," + "``last_login varchar(255)," + " PRIMARY KEY (uuid, name));";
+				sql.modifyQuery(query, false);
+			}
+		}
+	}
+
+	public static boolean isOpen() {
+		return isOpen;
+	}
 }
