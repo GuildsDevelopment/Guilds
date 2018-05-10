e6e8c5954ca2dda4c8d0afb8719c7749ddf6928e
Jacksonnn <jhuse01@gmail.com>
2018-05-09 22:48:11 -0400

    Databases
    Starting to create the files required for databases (storage) as well as added the plan.yml onto the github for ease.

---
+++ src/com/Jacksonnn/Guilds/storage/DBConnection.java
@@ -0,0 +1,25 @@
+package com.Jacksonnn.Guilds.storage;
+
+/*
+ * What needs to be stored:
+ * 		Guild:
+ * 			Name
+ * 			Members
+ * 			Chunks Claimed
+ * 			Leader
+ * 			Amount of Money (Coins)
+ * 			Moderators of Guild ("Apprentices")
+ * 			Trusted Guilds (Guilds that are allied with the current one)
+ * 			
+ * 		Players:
+ * 			Balance
+ * 			Guilds joined previously
+ * 			Current Guild
+ * 			Rank in Guild
+ * 			Date they joined the server
+ * 			Last online
+ */
+
+public class DBConnection {
+	
+}
