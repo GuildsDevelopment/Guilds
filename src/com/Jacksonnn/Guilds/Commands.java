41e79ba830bafe69fa2b99430d65dc1e219fe96f
RoboToe <32888997+RoboToe@users.noreply.github.com>
2018-05-14 20:14:00 -0500

    Update Commands.java
    Added some descriptions for the commands in the commented area.

--- src/com/Jacksonnn/Guilds/Commands.java
+++ src/com/Jacksonnn/Guilds/Commands.java
@@ -2,26 +2,28 @@ package com.Jacksonnn.Guilds;
 
 /*
  * Commands:
- * help
- * create <name>
- * delete
- * info <name>
+ * help - This will send a list of the following commands
+ * create <name> [<owner>] - This creates a new guild owned by the sender. 
+ * delete [<name>] - This deletes the sender's guild #Inner-Guild-Permissions needed
+ * info <name> - This will show anything about a guild, for example, the name, the age, the owner, the members, the coin balance. The name placeholder can either be a guild name or a player
  * 
- * claim
- * add <name>
- * unclaim
+ * claim [<name>] - This will claim land under the sender's faction #Inner-Guild-Permissions needed
+ * add <name> [<guild>] - This will give another Guild partial ownership of the land #Inner-Guild-Permissions needed
+ * remove <name> [<guild>] - This will take partial ownership of land from the said guild #Inner-Guild-Permissions needed
+ * unclaim [<guild>] - This will unclaim land under the sender's faction #Inner-Guild-Permissions needed
  * 
- * invite <player>
- * kick <player>
+ * invite <player> [<guild>] - This sends an request to allow a player to join the Guild #Inner-Guild-Permissions needed
+ * kick <player> [<guild>] - This removes a said player from the sender's guild #Inner-Guild-Permissions needed
  * 
- * chat
- * trust <player>
+ * chat - This toggles the channel. #Channels - Guild, Trusted, Public, War
+ * trust <name> [<guild>] [<guild>] - This marks alliance with another Guild. 
+ * attack <name> [<guild>] [<guild>] - This marks war with another Guild. 
  * 
- * attack <name>
+ * coins (deposit|withdrawal|add*|remove*) [<player>] [<guild>] - Manages coins
+ * job <join quit> - Joins or leaves a job. #You earn money from jobs on an interval f.ex I'd only earn a dollar in piggery killing a pig every 5 pigs, if the interval was set to 5
  * 
- * coins (deposit|withdrawal|add*|remove*)
- * job (join | quit)
- * 
+ * Admin Commands:
+ * admin - Bypasses all restrictions that normal players have as far as non-command actions
  */
 
 import org.bukkit.command.Command;
